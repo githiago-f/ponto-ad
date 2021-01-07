9361aad3d26b449a91ff11169a7a91fa25ce679a
githiago-f <52945005+githiago-f@users.noreply.github.com>
2021-01-07 04:04:41 +0000

    Deploying to gh-pages from  @ 0933b31dde5aea5b4b4f9eef7626857953c4e3ec 🚀

---
+++ precache-manifest.cc15ba09e72e8cd47fd7b036387652fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ca45768b5c7258c7f2c97c56e7fea4",
+    "url": "/ponto-ad/index.html"
+  },
+  {
+    "revision": "8b469c358e1c126d2ecd",
+    "url": "/ponto-ad/static/js/2.82484b15.chunk.js"
+  },
+  {
+    "revision": "b6f1c272977da08a105ed0daf86f7eb2",
+    "url": "/ponto-ad/static/js/2.82484b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37409d64cff0c3026ca1",
+    "url": "/ponto-ad/static/js/main.728c5b84.chunk.js"
+  },
+  {
+    "revision": "4e44d5b65ed884735da4",
+    "url": "/ponto-ad/static/js/runtime-main.93a0a83a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a796d18e64a18acbcaf72e2577d03b7d.js"
+  "/ponto-ad/precache-manifest.cc15ba09e72e8cd47fd7b036387652fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ponto-ad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
